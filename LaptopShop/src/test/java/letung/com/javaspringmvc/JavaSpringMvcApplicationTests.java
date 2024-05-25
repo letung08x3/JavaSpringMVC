23f2f0583ea563d5924151ff8bd1c419261d74f3
letung08x3 <157719039+letung08x3@users.noreply.github.com>
2024-05-25 17:13:20 +0700

    HelloworldSpringBoot

---
+++ LaptopShop/src/test/java/letung/com/javaspringmvc/JavaSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package letung.com.javaspringmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
